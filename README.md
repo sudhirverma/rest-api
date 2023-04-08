14a479298b68e3d557c49efd2f8aa8809872580a
vboxuser <vboxuser@ububtu.myguest.virtualbox.org>
2023-04-08 19:31:27 +0530

    added two more feature delete

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ python -m venv .venv
 pip install flask
 flask run
 ```
+
+### Image icon
+
+./img/Screenshot from 2023-04-08 15-16-45.png
--- app.py
+++ app.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 def get_stores():
     return {"stores": list(stores.values())}
 
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
@@ -26,6 +34,18 @@ def create_store():
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not deleted.")
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
 @app.post("/item")
 def create_item():
@@ -54,17 +74,27 @@ def create_item():
     items[item_id] = item
     return item, 201
 
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
     try:
-        return stores[store_id]
+        del items[item_id]
+        return {"message": "Item deleted."}
     except KeyError:
-        abort(404, message="Store not found.")
+        abort(404, message="Item not found.")
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
+    except KeyError:
+        abort(404, message="Item not found")
+
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
