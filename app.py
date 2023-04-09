7cf7e3a9bb0d0a08ff6eef308bf86ee4bc3b7d14
Sudhir Verma <sudhirvvermaa@gmail.com>
2023-04-09 15:50:29 +0530

    Added swagger and flask_smorest

--- app.py
+++ app.py
@@ -1,104 +1,31 @@
-from flask import Flask, request
-from db import items, stores
-import uuid
-from flask_smorest import abort
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
 app = Flask(__name__)
 
-@app.get("/store") #http://127.0.0.1:5000/store
-def get_stores():
-    return {"stores": list(stores.values())}
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.")
+# Exception occure hiden inside an extenstion of flask to propagate in main app to see it
+app.config["PROPAGATE_EXCEPTIONS"] = True
+# flask smorest configuration
+app.config["API_TITLE"] = "Stores REST API"
+#
+app.config["API_VERSION"] = "v1"
+# Standerd for API documentaion we test flask smorest to use 3.03
+app.config["OPENAPI_VERSION"] = "3.0.3"
+# to tell flask smorest where the root api is
+app.config["OPENAPI_URL_PREFIX"] = "/"
+# swagger-ui path this tell flask smorest to use swagger for api documentation 
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+# swagger-ui URL but it need to load swagger code from some were to display the documentation link of code below 
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://npmjs.com/package/swagger-ui-dist"
 
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    if "name" not in store_data:
-        abort(
-            400,
-            message="Bad request. Ensure 'name' is included in the JSON payload."
-        )
-    
-    for store in stores.values():
-        if store_data["name"] == store["name"]:
-            abort(400, message=f"Store already exits.")
-    
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
-
-@app.delete("/store/<string:store_id>")
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {"message": "Store deleted."}
-    except KeyError:
-        abort(404, message="Store not deleted.")
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if (
-        "price" not in item_data
-        or "store_id" not in item_data
-        or "name" not in item_data
-    ): 
-        abort(
-            400,
-            message="Bad request, Ensure 'price, 'store_id', and 'name' are included in the JSON payload."
-        )
-    for item in items.values():
-        if (
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
-        ):
-            abort(400, message=f"Item already exists.")
-
-    if item_data["store_id"] not in stores:
-        abort(404, message="Store not found.")
-    
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-    return item, 201
-
-@app.delete("/item/<string:item_id>")
-def delete_item(item_id):
-    try:
-        del items[item_id]
-        return {"message": "Item deleted."}
-    except KeyError:
-        abort(404, message="Item not found.")
-
-@app.put("/item/<string:item_id>")
-def update_item(item_id):
-    item_data = request.get_json()
-    if "price" not in item_data or "name" not in item_data:
-        abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
-
-    try:
-        item = items[item_id]
-        item |= item_data
-        return item
-    except KeyError:
-        abort(404, message="Item not found")
+# connect the flask smorest extenstion to the flask app
+api = Api(app)
 
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Item not found.")
+#
+api.register_blueprint(ItemBlueprint) # these are blp varible we defined in the resources file
+api.register_blueprint(StoreBlueprint)
