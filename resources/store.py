5f118985af2cacc27f3d9dd5cd4b2f24d6008523
Sudhir Verma <sudhirvvermaa@gmail.com>
2023-04-10 23:37:24 +0530

    Added response fromapi

--- resources/store.py
+++ resources/store.py
@@ -10,6 +10,7 @@ blp = Blueprint("stores", __name__, description="Operation on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -25,10 +26,12 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
         return {"stores": list(stores.values())}
 
     @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
     def post(self, store_data):
         store_data = request.get_json()
         
