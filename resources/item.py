0dba27bbdd172c932345997568ef391671730a65
Sudhir Verma <sudhirvvermaa@gmail.com>
2023-04-10 23:23:03 +0530

    added marshmallow

--- resources/item.py
+++ resources/item.py
@@ -4,6 +4,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
 
@@ -22,10 +23,8 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
 
         try:
             item = items[item_id]
@@ -40,17 +39,9 @@ class ItemList(MethodView):
     def get(self):
         return {"items": list(items.values())}
 
-    def post(self):
-        item_data = request.get_json()
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ): 
-            abort(
-                400,
-                message="Bad request, Ensure 'price, 'store_id', and 'name' are included in the JSON payload."
-            )
+    @blp.arguments(ItemSchema)
+    def post(self, item_data):
+        
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
@@ -58,8 +49,6 @@ class ItemList(MethodView):
             ):
                 abort(400, message=f"Item already exists.")
 
-        # if item_data["store_id"] not in stores:
-        #     abort(404, message="Store not found.")
         
         item_id = uuid.uuid4().hex
         item = {**item_data, "id": item_id}
--- resources/store.py
+++ resources/store.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreSchema
 
 blp = Blueprint("stores", __name__, description="Operation on stores")
 
@@ -27,13 +28,9 @@ class StoreList(MethodView):
     def get(self):
         return {"stores": list(stores.values())}
 
-    def post(self):
+    @blp.arguments(StoreSchema)
+    def post(self, store_data):
         store_data = request.get_json()
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'name' is included in the JSON payload."
-            )
         
         for store in stores.values():
             if store_data["name"] == store["name"]:
