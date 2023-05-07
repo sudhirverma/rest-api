ff3f14db2ebe070da04adfaf04acb0849a0a772c
sudhirverma <sudhirvvermaa@gmail.com>
2023-05-07 23:38:47 +0530

    Fix build

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-import secrets
 
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 from db import db
 from blocklist import BLOCKLIST
@@ -17,7 +17,7 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
-
+    load_dotenv()
 
     # Exception occure hiden inside an extenstion of flask to propagate in main app to see it
     app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -36,25 +36,25 @@ def create_app(db_url=None):
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-    # A connection string to our database. All database providers, such as MySQL, Postgres, 
-    # PostgreeSQL, SQLite or any other use a connection string that has all the necessary 
-    # information for a client to connect to the database. our Flask app is the client 
+    # A connection string to our database. All database providers, such as MySQL, Postgres,
+    # PostgreeSQL, SQLite or any other use a connection string that has all the necessary
+    # information for a client to connect to the database. our Flask app is the client
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     # slow down swlalchemy so we don't need it
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    # it initializes the Flask SQLAlchemy extension, giving it our Flask app so that it can 
+    # it initializes the Flask SQLAlchemy extension, giving it our Flask app so that it can
     # connect the Flsk app to SQLAlchemy.
     db.init_app(app)
-    
-    
+
+
     migrate = Migrate(app, db)
-    # flask db includes a subset of commands added by flask-migrate 
+    # flask db includes a subset of commands added by flask-migrate
     # that allow us to interact with Alembic
-    
+
     # when we do flask db init
     # it's going to create a migrations folder and inside it, there's a bunch of stuff
-    # The most important one is the versions folder, which is going to contain all the 
+    # The most important one is the versions folder, which is going to contain all the
     # changes that we make to our database over time.
     # alembic.ini is a configuration file.
     # env.py this is a script used by alembic to generate the migration files,
@@ -62,14 +62,14 @@ def create_app(db_url=None):
     # script.py.mako which is template for the migration files.
     
     # flask db migrate
-    # this ha generated the first migration and when you type flask db migrate 
-    # what happens is the alembic library will look at the existing database and 
-    # it will compare the existing database with the database defined by your models. 
+    # this ha generated the first migration and when you type flask db migrate
+    # what happens is the alembic library will look at the existing database and
+    # it will compare the existing database with the database defined by your models.
     # and it will create a script that allows you to go from one to the other
-    
+
     app.config["JWT_SECRET_KEY"] = "23877789178251158099025942000334528344" # secrets.SystemRandom().getrandbits(128)
     jwt = JWTManager(app)
-    
+
     # when we expect a fresh token, but we receive a non-fresh token.
     # So that's the needs fresh token loader
     @jwt.needs_fresh_token_loader
@@ -82,11 +82,11 @@ def create_app(db_url=None):
                 }
             )
         )
-    
+
     @jwt.token_in_blocklist_loader
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
         return jwt_payload["jti"] in BLOCKLIST
-    
+
     @jwt.revoked_token_loader
     def revoked_token_callback(jwt_header, jwt_payload):
         return (
@@ -97,7 +97,7 @@ def create_app(db_url=None):
                 }
             )
         )
-    
+
     @jwt.additional_claims_loader
     def add_claims_to_jwt(identity):
         # in create_access_token we receives and identity.
@@ -105,7 +105,7 @@ def create_app(db_url=None):
         if  identity == 1:
             return {"is_admin": True}
         return {"is_admin": False}
-    
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
@@ -117,7 +117,7 @@ def create_app(db_url=None):
             ),
             401,
         )
-        
+
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
         return (
@@ -129,7 +129,7 @@ def create_app(db_url=None):
             ),
             401,
         )
-    
+
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return (
@@ -146,10 +146,10 @@ def create_app(db_url=None):
     api = Api(app)
 
 
-    # 
+    #
     api.register_blueprint(ItemBlueprint) # these are blp varible we defined in the resources file
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
     api.register_blueprint(UserBlueprint)
-
+    
     return app
--- models/tag.py
+++ models/tag.py
@@ -7,7 +7,7 @@ class TagModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
-    
+
     store = db.relationship("StoreModel", back_populates="tags")
     # this is the secondary table that we've defined here as a table name.
     # SQLAlchemy will know that it has to go through this secondary table in order 
